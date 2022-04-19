2c163d0d23d62028e85f6c7529241dfa54a40396
DavidDalmauDieguez <david.dieguez13@gmail.com>
2022-04-19 10:40:57 +0200

    Creamos variables y metodos de fichero libros

--- BTC_Reus2022_PR03/src/App/libro.java
+++ BTC_Reus2022_PR03/src/App/libro.java
@@ -1,5 +1,61 @@
 package App;
 
 public class libro {
+	// Creamos las variables
+	private boolean isbn;
+	private String titulo;
+	private String autor;
+	private int numPaginas;
+	
+	// Creamos el constructor
+	public libro(boolean isbn, String titulo, String autor, int numPaginas) {
+		super();
+		this.isbn = isbn;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.numPaginas = numPaginas;
+	}
 
+	// Creamos los Getters y Setters
+	public boolean isIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(boolean isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public int getNumPaginas() {
+		return numPaginas;
+	}
+
+	public void setNumPaginas(int numPaginas) {
+		this.numPaginas = numPaginas;
+	}
+
+	// Creamos el toString
+	@Override
+	public String toString() {
+		return "libro [isbn=" + isbn + ", titulo=" + titulo + ", autor=" + autor + ", numPaginas=" + numPaginas + "]";
+	}
+	
+	
+	
+	
 }
